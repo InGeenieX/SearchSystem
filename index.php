26e89f9c3a3617dbfebe94fec116aba9309ed14a
Denis Teyssier <admin@mascali.ovh>
2017-11-02 20:21:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <head>
+<link rel="stylesheet" href="https://search.mascacorp.fr/inc/css/style.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
 <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
